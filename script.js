252c540e6386dd209a04aafcedcf2d32428937a5
JohnatanChagas <johnatan_pereira@hotmail.com>
2023-05-02 10:13:50 -0300

    primeiro commit replica Netflix

---
+++ script.js
@@ -0,0 +1,15 @@
+let botao = document.getElementById("botao-pergunta")
+let menu = document.getElementById("pergunta")
+let imagem = document.getElementById("imagem-pergunta")
+
+function clickBotao() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block"
+        imagem.id = "imagem-pergunta-girar"
+    }else{
+        menu.style.display = "none"
+        imagem.id = "imagem-pergunta"
+    }
+}
+
+botao.addEventListener("click", clickBotao)
